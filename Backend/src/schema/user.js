2fcf148043efb5b626c1b21739926082e0416e01
Rupam <rupammondal564@gmail.com>
2024-12-12 19:03:44 +0530

    Userschema added

---
+++ Backend/src/schema/user.js
@@ -0,0 +1,37 @@
+import mongoose, { mongo } from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required:[true , "Email required"],
+        unique:true,
+        validate: {
+            validator: function (emailValue) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailValue);
+            },
+            message: 'Invalid email format'
+        }
+    },
+    username:{
+        type:String,
+        minlength:3,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true , "password is required"]
+    },
+    avatar:{
+        type:String
+    }
+} , {timestamps:true});
+
+userSchema.pre('save' , function (next){
+    const user = this;
+    user.avatar = `https://robohash.org/${user.username}`;
+    next();
+})
+
+const User = mongoose.model("User" , userSchema);
+
+export default User;
