eecbed346403c4131c090eed3f899b0e7137bd3c
Rupam <rupammondal564@gmail.com>
2024-12-16 08:12:08 +0530

    update

--- Backend/src/schema/message.js
+++ Backend/src/schema/message.js
@@ -19,3 +19,7 @@ const messageSchema = new mongoose.Schema({
         required:true
     }
 } , {timestamps:true});
+
+
+const Message = mongoose.model('Message' , messageSchema);
+export default Message;
