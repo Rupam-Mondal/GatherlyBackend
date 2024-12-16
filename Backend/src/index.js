4fde33bf0f23db53d20f8915aa953de406b2d2e7
Rupam <rupammondal564@gmail.com>
2024-12-16 12:53:18 +0530

    Update

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,8 +5,12 @@ import { isAuthenticate } from './middlewares/authmiddleware.js';
 import workspaceRouter from './routes/workspaceRouter.js';
 import channelRouter from './routes/channelRouter.js';
 import messageRouter from './routes/messageRouter.js';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -22,7 +26,15 @@ app.get('/ping', isAuthenticate , (req , res) => {
     })
 });
 
-app.listen(3000 , () => {
+io.on('connection' , (socket) => {
+    socket.on('message' , (data) => {
+        setInterval(() => {
+            io.emit('new message' , data);
+        } , 3000);
+    })
+})
+
+server.listen(3000 , () => {
     console.log("Server is running");
     connectDb();
 })
