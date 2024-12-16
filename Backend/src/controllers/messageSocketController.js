b9c25a9d07d15cec25a9b6055311ce51208c1889
Rupam <rupammondal564@gmail.com>
2024-12-16 14:02:45 +0530

    Group message feature added

--- Backend/src/controllers/messageSocketController.js
+++ Backend/src/controllers/messageSocketController.js
@@ -1,11 +1,14 @@
 import { createMessageService } from "../service/messageService.js";
 
 export default function messageSocketController(io , socket){
-    socket.on('newMessage', createMessageHandler);
+    socket.on('newMessage', async (data , cb) => {
+        await createMessageHandler(data , cb , io , socket);
+    });
 }
 
-async function createMessageHandler(data , cb){
+async function createMessageHandler(data , cb , io , socket){
     const msgResponse = await createMessageService(data);
+    io.emit("messageToAll" , data);
     cb({
         success:true
     })
