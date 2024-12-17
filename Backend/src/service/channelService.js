db0ba9618a5eae015b0d4d2a9445c703e475ca7b
Rupam <rupammondal564@gmail.com>
2024-12-17 09:35:57 +0530

    Messages fetched alone with channel api updated

--- Backend/src/service/channelService.js
+++ Backend/src/service/channelService.js
@@ -1,10 +1,18 @@
 import { getChannelById } from "../repositories/channelRepository.js";
+import { getMessagesOfChannel } from "../repositories/messageRepository.js";
 
 export async function  getChannelByIdService(channelId , userId){
     try {
         const response = await getChannelById(channelId);
-        console.log(response);
-        return response;
+        const messages = await getMessagesOfChannel(channelId, 0, 10);
+        return {
+            _id:response._id,
+            name:response.name,
+            workspace:response.workspace,
+            messages:messages,
+            createdAt:response.createdAt,
+            updatedAt:response.updatedAt
+        }
     } catch (error) {
         console.log("Something went wrong");
         return null;
