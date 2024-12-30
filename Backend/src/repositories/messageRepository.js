a3836ae213f0d35857a11936573156280f4f3213
Rupam <rupammondal564@gmail.com>
2024-12-30 10:24:08 +0530

    Update in socket layer

--- Backend/src/repositories/messageRepository.js
+++ Backend/src/repositories/messageRepository.js
@@ -3,7 +3,12 @@ import Message from "../schema/message.js";
 
 export async function createMessage(messageObject){
     try {
-        const response = await Message.create(messageObject);
+        const object = {
+            body:messageObject.body,
+            senderId:messageObject.senderId.id,
+            channelId:messageObject.channelId
+        }
+        const response = await Message.create(object);
         return response;
     } catch (error) {
         console.log("Something went wrong");
@@ -14,7 +19,7 @@ export async function getMessagesOfChannel(channelId , offset , pageSize){
     try {
         const response = await Message.find({
             channelId:channelId
-        }).sort({ createdAt: -1 }).skip(offset).limit(pageSize).populate('senderId' , 'username email avatar');
+        }).sort({ createdAt: -1 }).skip(offset).limit(pageSize).populate('senderId');
         return response;
     } catch (error) {
         console.log("Something went wrong");
